0d1df81de40d764e2397c7e068cd7f165b902d6b
Gavrilovph <vladimir.gavrilov.ph@yandex.ru>
2023-08-16 18:00:30 +0400

    Edit script.js
    Created variables

---
+++ source/js/script.js
@@ -0,0 +1,13 @@
+let age = 33;
+let animal = "cat";
+let loudMusic = true;
+let allDayLongProgramming = true;
+let sleepHours = 8;
+let mind = "open";
+
+console.log (Number(age), String(age), Boolean(age));
+console.log (Number(animal), String(animal), Boolean(animal));
+console.log (Number(loudMusic), String(loudMusic), Boolean(loudMusic));
+console.log (Number(allDayLongProgramming), String(allDayLongProgramming), Boolean(allDayLongProgramming));
+console.log (Number(sleepHours), String(sleepHours), Boolean(sleepHours));
+console.log (Number(mind), String(mind), Boolean(mind));
